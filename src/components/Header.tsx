ef2aeafd2ddef5b5d93664fab08d31caef880af1
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2025-01-20 07:43:33 +0100

    Enhance Header component: improve title formatting and adjust styles for better responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,10 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-innerWrapper">
-        <h1 className="header-title">Stable Diffusion · Image Synthesis</h1>
+        <h1 className="header-title">
+          Stable Diffusion. <br />
+          Image Synthesis
+        </h1>
         <p className="header-subtitle">
           Collection of images generated through Stable Diffusion
         </p>
