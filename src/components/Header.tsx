a63fecdd1dde9bd1592041f050bb39157418e3c0
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2025-02-01 20:39:42 +0100

    Add Menu component with navigation; integrate into Header and add toggle functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,16 @@
+import { useState } from 'react'
+
+import ButtonIcon from '@components/ButtonIcon'
+import Menu from '@components/Menu'
+
+import IconMenu from '@icons/IconMenu'
+
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleCloseMenu = () => setMenuOpen(false)
+  const toggleMenu = () => setMenuOpen(prevState => !prevState)
+
   return (
     <header className="header">
       <div className="header-innerWrapper">
@@ -9,6 +21,17 @@ export default function Header() {
         <p className="header-subtitle">
           Collection of images generated through Stable Diffusion
         </p>
+
+        <ButtonIcon
+          onClick={toggleMenu}
+          ariaLabel={menuOpen ? 'Close menu' : 'Open menu'}
+          icon={<IconMenu />}
+          size="sm"
+          type="secondary"
+          extraClass="header-menuButton"
+        />
+
+        <Menu open={menuOpen} onClose={handleCloseMenu} />
       </div>
     </header>
   )
